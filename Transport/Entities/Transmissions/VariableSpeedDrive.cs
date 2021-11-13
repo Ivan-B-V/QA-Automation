be46ef4a669b326920fda47381c9f3a3ab9fe73e
Ivan <ivan.buyeuskii@gmail.com>
2021-11-13 20:59:27 +0300

    Add VariableSpeedDrive.cs

---
+++ Transport/Entities/Transmissions/VariableSpeedDrive.cs
@@ -0,0 +1,19 @@
+﻿using Transport.Enums;
+
+namespace Transport.Entities.Transmissions
+{
+    class VariableSpeedDrive : Transmission
+    {
+        public VariableSpeedDrive(string manufacturer)
+        {
+            Gears = 1;
+            Manufacturer = manufacturer;
+            Type = TransmissionTypes.VariableSpeedDrive;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
