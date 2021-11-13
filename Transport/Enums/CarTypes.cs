136166768c2b7db38a50b9c457f2fcbf2222aa2b
Ivan <ivan.buyeuskii@gmail.com>
2021-11-13 20:35:05 +0300

    Add CarTypes enum

---
+++ Transport/Enums/CarTypes.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Transport.Enums
+{
+    enum CarTypes
+    {
+        Sedan,
+        Minivan,
+        MPV,
+        Liftback,
+        Coupe,
+        Hatchback,
+        Pickup,
+        SUV
+    }
+}
