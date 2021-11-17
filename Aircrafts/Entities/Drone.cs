964af1438789d3b28a4d484f0480d392da530f19
Ivan <ivan.buyeuskii@gmail.com>
2021-11-17 16:50:14 +0300

    Add drone base class

---
+++ Aircrafts/Entities/Drone.cs
@@ -0,0 +1,15 @@
+﻿using Aircrafts.Interfaces;
+
+namespace Aircrafts.Entities
+{
+    /// <summary>
+    /// Represents base behavior and properties intance like a Drone.
+    /// </summary>
+    abstract class Drone : IFlyable
+    {
+        public abstract void FlyTO(Point3D point);
+
+        public abstract double GetFlyTime(Point3D point);
+
+    }
+}
