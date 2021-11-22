dee9b061a81963ebefb9f9569a86d6911eb2df1c
Ivan <ivan.buyeuskii@gmail.com>
2021-11-22 15:55:00 +0300

    Add comments

--- NumberSystems/NumberSystemsConverter.cs
+++ NumberSystems/NumberSystemsConverter.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-
+﻿
 namespace NumberSystems
 {
+    /// <summary>
+    /// Converts a base data type to another base data type. 
+    /// </summary>
     class NumberSystemsConverter
     {
         private const int startNumberSystem = 2;
         private const int endNumberSystem = 20;
-       
+
+        /// <summary>
+        /// Converts a base data type to another base data type from 2 to 20 base. 
+        /// </summary>
+        /// <param name="inputValue"> Integer value to convert. </param>
+        /// <param name="numberSystem"> Base of data type to convert. </param>
+        /// <returns> Converted input value to the set type in string format. </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException"> Input param numberSystem should be between 2 and 20. </exception>
         public static string ConvertFromIntTo(int inputValue, int numberSystem)
         {
 
             if (numberSystem < startNumberSystem || numberSystem > endNumberSystem)
             {
-                throw new Exception($"Invalid number system. Should be between {startNumberSystem} and {endNumberSystem}.");
+                throw new System.ArgumentOutOfRangeException(nameof(numberSystem), $"Invalid number system. Should be between {startNumberSystem} and {endNumberSystem}.");
             }
 
-            StringBuilder binNumberStr = new StringBuilder();
+            System.Text.StringBuilder binNumberStr = new();
 
             bool isNegative = false;
             if (inputValue < 0)
@@ -30,7 +36,7 @@ namespace NumberSystems
             do
             {
                 int divRemaider = inputValue % numberSystem;
-                if (divRemaider <= 10)
+                if (divRemaider < 10)
                 {
                     binNumberStr.Insert(0, divRemaider);
                 }
@@ -38,8 +44,6 @@ namespace NumberSystems
                 {
                     binNumberStr.Insert(0, (char)(55 + divRemaider)); // ACII 65(A)
                 }
-                //binNumberStr.Insert(0, divRemaider < 10 ? (char)divRemaider : (char)(55 + divRemaider));
-                //binNumberStr.Insert(0, divRemaider < 10 ? divRemaider : Convert.ToChar(55 + divRemaider));
                 inputValue /= numberSystem;
             } while (inputValue != 0);
 
--- NumberSystems/Program.cs
+++ NumberSystems/Program.cs
@@ -6,14 +6,31 @@ namespace NumberSystems
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length != 2)
             {
                 Console.WriteLine("First argument: decimal value, Second: number system.");
                 return;
-
             }
 
-            Console.WriteLine(NumberSystemsConverter.ConvertFromIntTo(Convert.ToInt32(args[0]), Convert.ToInt32(args[1])));
+            try
+            {
+                int value = Convert.ToInt32(args[0]);
+                int dataBase = Convert.ToInt32(args[1]);
+                Console.WriteLine(NumberSystemsConverter.ConvertFromIntTo(value, dataBase));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
