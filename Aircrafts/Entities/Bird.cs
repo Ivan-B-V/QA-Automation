eb2949cad1670cef6734ef11f1d27ce4bbae11ad
Ivan <ivan.buyeuskii@gmail.com>
2021-11-26 21:12:20 +0300

    Big refactoring

--- Aircrafts/Entities/Bird.cs
+++ Aircrafts/Entities/Bird.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using Aircrafts.Interfaces;
+﻿using Aircrafts.Interfaces;
 
 namespace Aircrafts.Entities
 {
+    /// <summary>
+    /// Represents Bird instance.
+    /// </summary>
     class Bird : IFlyable
     {
-        public Point3D position;
-        private double maxSpeed = 20;
+ 
+        private Point3D position;
+
+        private const double maxAltitude = 7;
+
+        private const double maxSpeed = 20;
+
         private double speed;
-        private double maxAltitude = 7;
 
-        public double Speed
-        {
-            get => speed;
-            protected set => speed = value > maxSpeed ? maxSpeed : value;
-        }
-
+        /// <summary>
+        /// Posistion property.
+        /// </summary>
+        ///<value> Property Position represents current bird's position</value> 
         public Point3D Position
         {
             get => position;
-            protected set 
+            private set
             {
                 if (value.Z > maxAltitude)
                 {
@@ -28,19 +32,69 @@ namespace Aircrafts.Entities
             }
         }
 
-        public Bird(Point3D position)
+        /// <summary>
+        /// Speed property.
+        /// </summary>
+        ///<value> Property Speed represents current bird's speed</value> 
+        public double Speed
         {
-            Position = position;
-            Speed = (uint)new Random().Next((int)maxSpeed);
+            get => speed;
+            private set => speed = value > maxSpeed ? maxSpeed : value;
         }
 
+        /// <summary>
+        /// Max bird's possible altitude.
+        /// </summary>
+        public double MaxAltitude
+        {
+            get => maxAltitude;
+        }
+
+        /// <summary>
+        /// Max bird's speed.
+        /// </summary>
+        public double MaxSpeed
+        {
+            get => maxSpeed;
+        }
+
+        /// <summary>
+        /// Initialise instance of Aircrafts.Entities.Bird class.
+        /// </summary>
+        /// <param name="point"> Initial bird's posistion. </param>
+        public Bird(Point3D point)
+        {
+            FlyTO(point);
+            Speed = (uint)new System.Random().Next((int)maxSpeed);
+        }
+
+        /// <exception cref="System.ArgumentNullException"> Throws when point is null. </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException"> Throws when point altitude is more than MaxAltitude. </exception>
         public void FlyTO(Point3D point)
         {
+            if(point is null)
+            {
+                throw new System.ArgumentNullException(nameof(point), "Bird cannot flight to the emptiness.");
+            }
+            if (point.Z > MaxAltitude)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(point), $"Max possible bird's altitude is {MaxAltitude}");
+            }
             Position = point;
         }
 
+        /// <exception cref="System.ArgumentNullException"> Throws when point is null. </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException"> Throws when point altitude is more than MaxAltitude. </exception>
         public double GetFlyTime(Point3D point)
         {
+            if (point is null)
+            {
+                throw new System.ArgumentNullException(nameof(point), "Bird cannot flight to the emptiness.");
+            }
+            if (point.Z > maxAltitude)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(point), $"Max possible bird's altitude is {maxAltitude}");
+            }
             double time = Position.Distance(point) / speed;
 
             return time;
